9b8086f9dcfd755d3f34d2f0654b16e222c8b9e4
Vatsal Bhanderi <vbhanderi6@gmail.com>
2024-02-28 16:37:22 -0800

    Initial Commit

---
+++ Scores/ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoresApp.swift
+//  Scores
+//
+//  Created by Vatsal Bhanderi on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
